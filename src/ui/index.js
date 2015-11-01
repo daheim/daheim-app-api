1b1572c0ae1f1838562be345d36071babbd99efe
Gergo Ertli <egergo@gmail.com>
2015-11-01 12:56:56 +0200

    Only using sourcemaps when localStorage.debug contains stack

--- src/ui/index.js
+++ src/ui/index.js
@@ -2,7 +2,10 @@ import 'babel-core/polyfill';
 import Promise from 'bluebird';
 
 Promise.noConflict();
-require('source-map-support').install();
+
+if (global.localStorage && typeof global.localStorage.debug === 'string' && global.localStorage.debug.indexOf('stack') !== -1) {
+	require('source-map-support').install();
+}
 
 require('./core');
 require('./second');
