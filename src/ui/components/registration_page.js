783b343932c7d315438b97a26439599405b85d2f
Gergo Ertli <egergo@gmail.com>
2016-02-16 17:38:56 +0200

    Navigating away after registration

--- src/ui/components/registration_page.js
+++ src/ui/components/registration_page.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import {RegistrationForm} from './registration_form';
+import {history} from './history';
 
 export class RegistrationPage extends React.Component {
+
+	handleLogin = () => {
+		history.push('/');
+	};
 
 	render() {
 		return (
 			<div style={{maxWidth: 400, margin: '0 auto', padding: '80px 10px'}}>
 				<div style={{background: 'rgba(255,255,255,0.9)', borderRadius: 10, padding: 20}}>
-					<RegistrationForm />
+					<RegistrationForm onLogin={this.handleLogin} />
 				</div>
 			</div>
 		);
