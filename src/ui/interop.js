115eded3fc8aa9e68dee3e952832840ffed3e9d7
Gergo Ertli <egergo@gmail.com>
2016-02-16 17:38:20 +0200

    More Angular <-> React interop variables

--- src/ui/interop.js
+++ src/ui/interop.js
@@ -4,6 +4,8 @@ export class Interop {
 	init() {
 		let injector = this.injector = angular.element(document.body).injector();
 		this.auth = injector.get('auth');
+		this.$rootScope = injector.get('$rootScope');
+		this.$compile = injector.get('$compile');
 	}
 
 }
