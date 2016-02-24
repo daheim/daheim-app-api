0d5bf98df99b59690e8c54371c52a8951680e8e0
Gergo Ertli <egergo@gmail.com>
2016-02-24 11:37:32 +0200

    API to upload lesson review

--- src/server/api/encounter.js
+++ src/server/api/encounter.js
@@ -1,7 +1,9 @@
 import {Router} from 'express';
+import Promise from 'bluebird';
+import MongooseError from 'mongoose/lib/error';
 
 import tokenHandler from '../token_handler';
-import {Encounter, User} from '../model';
+import {Encounter, User, UserRating} from '../model';
 
 const PING_THRESHOLD = 2.5 * 60 * 1000;
 
@@ -11,6 +13,19 @@ export class EncounterApi {
 		this.router = Router();
 		this.router.use(tokenHandler.auth);
 		this.router.get('/', this.handler(this.getEncounters));
+		this.router.get('/:encounterId', this.handler(this.getEncounter));
+		this.router.post('/:encounterId', this.handler(this.saveEncounter));
+
+		this.router.use((err, req, res, next) => {
+			if (err instanceof MongooseError) {
+				res.status(400).send({
+					error: 'validation',
+					message: err.toString(),
+				});
+			} else {
+				next(err);
+			}
+		});
 	}
 
 	async getEncounters({user}) {
@@ -57,6 +72,82 @@ export class EncounterApi {
 		}
 
 		return result;
+	}
+
+	async getEncounter({user, params: {encounterId}}) {
+		let encounter = await Encounter.findOne({
+			_id: encounterId,
+			'participants.userId': user.id,
+		});
+		if (!encounter) { throw new Error('not found'); }
+
+		let me;
+		let partner;
+		encounter.participants.forEach(p => {
+			if (p.userId === user.id) {
+				me = p;
+			} else {
+				partner = p;
+			}
+		});
+
+		let [myRating, partnerRating] = await Promise.all([
+			UserRating.findOne({from: me.userId, userId: partner.userId}).select('overall language -_id'),
+			UserRating.findOne({from: partner.userId, userId: me.userId}).select('overall language -_id'),
+		]);
+
+		let now = Date.now();
+		let length = encounter.length;
+		if (!length && encounter.ping < now - PING_THRESHOLD) {
+			length = encounter.ping - encounter.date;
+		}
+
+		let partnerUser = await User.findById(partner.userId).select('profile.name');
+		return {
+			id: encounter.id,
+			date: encounter.date.getTime(),
+			length,
+			myReview: me.review,
+			partnerReview: partner.review,
+			partnerId: partner.userId,
+			partnerName: partnerUser ? partnerUser.profile.name : undefined,
+			myRating,
+			partnerRating,
+		};
+	}
+
+	async saveEncounter({user, body, params: {encounterId}}) {
+		let encounter = await Encounter.findOne({
+			_id: encounterId,
+			'participants.userId': user.id,
+		});
+		if (!encounter) { throw new Error('not found'); }
+
+		let me;
+		let partner;
+		encounter.participants.forEach(p => {
+			if (p.userId === user.id) {
+				me = p;
+			} else {
+				partner = p;
+			}
+		});
+
+		if (body.myRating) {
+			let userRating = await UserRating.findOne({from: me.userId, userId: partner.userId});
+			if (!userRating) { userRating = new UserRating({from: me.userId, userId: partner.userId}); }
+			userRating.overall = body.myRating.overall;
+			userRating.language = body.myRating.language;
+			await userRating.save();
+		}
+
+		if (body.myReview) {
+			let {overall, words, good, bad} = body.myReview;
+			me.review = {overall, words, good, bad};
+			await encounter.save();
+		}
+
+		return await this.getEncounter({user, params: {encounterId}});
 	}
 
 	handler(fn) {
--- src/server/model/encounter.js
+++ src/server/model/encounter.js
@@ -14,3 +14,15 @@ let EncounterSchema = new Schema({
 
 let Encounter = mongoose.model('Encounter', EncounterSchema);
 export {Encounter};
+
+
+let UserRatingSchema = new Schema({
+	userId: {type: Schema.Types.ObjectId, required: true},
+	from: {type: Schema.Types.ObjectId, required: true},
+	overall: {type: Number, min: 1, max: 5, required: true},
+	language: {type: Number, min: 1, max: 5},
+});
+
+UserRatingSchema.index({userId: 1, from: 1}, {unique: true});
+
+export let UserRating = mongoose.model('UserRating', UserRatingSchema);
