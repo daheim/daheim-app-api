b57d20e126c6bad1396d6d77971cd359353a4e92
Gergo Ertli <egergo@gmail.com>
2016-02-29 14:14:16 +0200

    Angular interop extended

--- src/ui/interop.js
+++ src/ui/interop.js
@@ -1,11 +1,14 @@
+import {EventEmitter} from 'events';
 
-export class Interop {
+export class Interop extends EventEmitter {
 
 	init() {
 		let injector = this.injector = angular.element(document.body).injector();
 		this.auth = injector.get('auth');
+		this.config = injector.get('config');
 		this.$rootScope = injector.get('$rootScope');
 		this.$compile = injector.get('$compile');
+		this.emit('ready');
 	}
 
 }
