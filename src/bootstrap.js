044904015d77b64817c4f8180a483e41f4b57294
Gergo Ertli <egergo@gmail.com>
2016-05-23 17:34:25 +0300

    Using Bluebird as the default Promise implementation

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,18 +1,20 @@
 import 'babel-polyfill'
 import SourceMapSupport from 'source-map-support'
 import './reporter'
-import Promise from 'bluebird'
+import Bluebird from 'bluebird'
 import bcrypt from 'bcryptjs'
+
+require('babel-runtime/core-js/promise').default = Bluebird
 
 SourceMapSupport.install()
 
-Promise.config({
+Bluebird.config({
   longStackTraces: true,
   warnings: process.env.NODE_ENV === 'development'
 })
+Bluebird.promisifyAll(bcrypt)
 
 process.on('unhandledRejection', function (reason) {
   console.error('Unhandled rejection:', reason.stack) // eslint-disable-line no-console
 })
 
-Promise.promisifyAll(bcrypt)
