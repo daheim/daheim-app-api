3e154c9aea3219dfeb360a1eb92c30ff181bab18
Gergo Ertli <egergo@gmail.com>
2016-05-23 08:48:31 +0300

    Finalized avatars

--- src/avatars.js
+++ src/avatars.js
@@ -1,5 +1,15 @@
 
 export default {
-  default: 'http://www.creativemediaresearch.com/wp-content/uploads/2013/04/woman.jpg',
-  scotland: 'http://gdpit.com/avatars_pictures/buildings/gdpit_com_55373963_5.jpg'
+  avatar1: 'https://assets.daheimapp.de/public/assets/avatar-1.svg',
+  avatar2: 'https://assets.daheimapp.de/public/assets/avatar-2.svg',
+  avatar3: 'https://assets.daheimapp.de/public/assets/avatar-3.svg',
+  avatar4: 'https://assets.daheimapp.de/public/assets/avatar-4.svg',
+  avatar5: 'https://assets.daheimapp.de/public/assets/avatar-5.svg',
+  avatar6: 'https://assets.daheimapp.de/public/assets/avatar-6.svg',
+  avatar7: 'https://assets.daheimapp.de/public/assets/avatar-7.svg',
+  avatar8: 'https://assets.daheimapp.de/public/assets/avatar-8.svg',
+  avatar9: 'https://assets.daheimapp.de/public/assets/avatar-9.svg',
+  avatar10: 'https://assets.daheimapp.de/public/assets/avatar-10.svg',
+  avatar11: 'https://assets.daheimapp.de/public/assets/avatar-11.svg',
+  avatar12: 'https://assets.daheimapp.de/public/assets/avatar-12.svg'
 }
